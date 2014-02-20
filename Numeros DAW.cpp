74dacb84cca836332c2a0ee782c6eeab6a19a49a
Haru <haru195@gmail.com>
2014-02-20 21:45:08 +0100

    Terminado DAW

--- Numeros DAW.cpp
+++ Numeros DAW.cpp
@@ -2,62 +2,63 @@
 
 
 
-int primo(int num){//15
+int primo(int num){//Funcion para comprobar si el numero es primo
 int ceros=0,cont=1,resto;
 
          if(num==1){
-            return num;
+            return num;//en caso del numero ser 1 devolveria el numero
          }else{
               do{
-                resto=num%cont;//15/1
+                resto=num%cont;
                  if(resto==0){
-                    ceros++;//1
+                    ceros++;
                         }
-                cont++;//2
+                cont++;
 
             }while(cont<=num);
-            if(ceros==2){
-                return num;
+            if(ceros==2){//comprueba si es primo
+                return num;//devuelve el numero
             }else{
-                return 0;}
+                return 0;}//en caso de no serlo nos devuelve un 0
          }
 }
 
-int numerosDaw(int valor){//4
+int numerosDaw(int valor){
 int x,cont=1,resto,total=1;
-            while(cont<valor){//1<4--2<4
-            resto= valor % cont;
+            while(cont<valor){//Bucle que nos permite comprobar si es un numero DAW
+            resto= valor % cont;// dividimos todas las veces por los numeros desde 1 hasta el mismo
             if(resto==0){
-                x=primo(cont);
-                total=total*x;
+                x=primo(cont);// comprueba que su divisor es un numero primo
+                total=total*x;//guardariamos en total todos los valores de los divisores
             }
-            cont++;// 2
+            cont++;// aumentamos el contador
             }
-            if(total!=0){
-                return valor;
+            if(total!=0){// comprobamos el total,ya que si existia un divisor que no era primo
+                        // al haberle devuelto un cero, total valdría cero.
+                return valor;//
             }else{
 
-                return total;
+                return total;// devolvemos total, que lo necesitaremos en la función main.
                 }
 }
 
 
 int main(){
 int numero,x =0,y=1,cnt=1;//5
-printf("Dame un numero: ");
-scanf("%i",&numero);//5
+printf("Dame un numero: ");// pedimos el numero a comprobar si es daw o no
+scanf("%i",&numero);
 
-    x= numerosDaw(numero);
-    if(x!=0){
+    x= numerosDaw(numero);// guardamos en x el resultado de daw
+    if(x!=0){// comprobamos mediante los returns para saber si es un numero DAw
         printf("El numero %i es un numero DAW y los numeros Daw anteriores a el son: \n",x);
-        do{
-                y= numerosDaw(cnt);
-                if(y!=0){
+        do{// bucle para imprimir los daw anteriores
+                y= numerosDaw(cnt);//llamamos a la funcion mandandole el numero y
+                if(y!=0){//comprobamos como antes e imprimimos solo los numeros DAW
             printf("%i ",y);
 
             }
             cnt++;
-        }while(cnt<numero);
+        }while(cnt<numero);//condicion de salida del bucle
     }
 
 
